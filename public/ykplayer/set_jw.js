8beaf26edd491dc420d917c94044183df012476a
刘梦晨 <mengchen.liu@corp.elong.com>
2016-01-15 18:01:39 +0800

    修复event发生时全局变量不准的js bug

--- public/ykplayer/set_jw.js
+++ public/ykplayer/set_jw.js
@@ -3,9 +3,10 @@ window.set_jwplayer = function(src){
   var ww = document.getElementById('ykplayer0').clientWidth;
   var hh = ww*0.5625;
   var tt = 'mp4';
+  var pcount = PCOUNT;
   if(src.indexOf('/flv/') > 0)
     tt = 'flv';
-  var jwp = jwplayer('ykplayer' + PCOUNT)
+  var jwp = jwplayer('ykplayer' + pcount)
     jwp.setup({
       file:src,
       type:tt,
@@ -16,23 +17,24 @@ window.set_jwplayer = function(src){
       {type: 'html5'}
     ],
     });
-  if(PCOUNT == 0){
+  if(pcount == 0){
     jwp.on('error',function(e){
       if(e.message == 'Error loading media: File could not be played'){
         $('#ykplayer0').append("<div class='hidden'><iframe src='http://www.youku.com'></iframe></div>");
         $('div.jw-title-primary.jw-reset').html("服务器开小差了，请刷新页面试一试~");
-        $('#source_link_p_' + PCOUNT).removeClass('hidden');
-        var lk = $('#source_link_a_' + PCOUNT).data('original');
-        $('#source_link_a_' + PCOUNT).attr({href: lk});
+        alert(pcount);
+        $('#source_link_p_' + pcount).removeClass('hidden');
+        var lk = $('#source_link_a_' + pcount).data('original');
+        $('#source_link_a_' + pcount).attr({href: lk});
       }
     });
   }
   else{
     jwp.on('error',function(e){
       if(e.message == 'Error loading media: File could not be played'){
-        $('#source_link_p_' + PCOUNT).removeClass('hidden');
-        var lk = $('#source_link_a_' + PCOUNT).data('original');
-        $('#source_link_a_' + PCOUNT).attr({href: lk});
+        $('#source_link_p_' + pcount).removeClass('hidden');
+        var lk = $('#source_link_a_' + pcount).data('original');
+        $('#source_link_a_' + pcount).attr({href: lk});
       }
     });
   }
